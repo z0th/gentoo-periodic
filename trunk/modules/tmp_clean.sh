4481b8788d659e1608bec9d4979c3eeff31d01db
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2009-09-10 17:57:40 +0000

    tmp_clean.sh: getting proper file/dir names properly ignored.

--- trunk/modules/tmp_clean.sh
+++ trunk/modules/tmp_clean.sh
@@ -5,14 +5,14 @@
 # tmp_clean.sh - clean out /tmp
 #
 
-# verbosicty and file removal on/off
+# verbosity and file removal on/off
 RM_FILES="no"
 CHATTY="yes"
 # dirs to look at 
 TMP_ROOT="/tmp /var/tmp"
 # number days to ignore, files to ignore
 DAYS=3
-IGNORE_NAMES="X*-lock"
+IGNORE_NAMES="( -not -iname "X.*-lock" -o -not -iname "etc-update*" )"
 # args for files
 FILE_ARGS="-depth -type f"
 FILE_ARGS="${FILE_ARGS} -atime +$DAYS -ctime +$DAYS -mtime +$DAYS"
@@ -20,9 +20,9 @@ FILE_ARGS="${FILE_ARGS} -atime +$DAYS -ctime +$DAYS -mtime +$DAYS"
 DIR_ARGS="-depth -empty -type d"
 DIR_ARGS="${DIR_ARGS} -atime +$DAYS -ctime +$DAYS -mtime +$DAYS"
 
-if [ -n $IGNORE_NAMES ]; then 
-	IGNORE_FILE_ARGS="! -name ${IGNORE_NAMES}"
-	IGNORE_DIR_ARGS="! -name ${IGNORE_NAMES}"
+if [ -n "${IGNORE_NAMES}" ]; then 
+	IGNORE_FILE_ARGS="${IGNORE_NAMES}"
+	IGNORE_DIR_ARGS="${IGNORE_NAMES}"
 fi
 
 # determine if we are removing files
@@ -35,6 +35,7 @@ case "$RM_FILES" in
 	;;
 	*)
 		echo " ERROR! Are we removing files?"
+		echo ""
 		exit 1
 	;;
 esac
@@ -48,7 +49,8 @@ case "$CHATTY" in
 		VERBOSE=""
 	;;
 	*)
-		echo "ERROR! Do you want verbose output?"
+		echo " ERROR! Do you want verbose output?"
+		echo ""
 		exit 1
 	;; 	
 esac
@@ -56,7 +58,10 @@ esac
 # file searching/removal done here
 for dir in $TMP_ROOT; do 
 	if [ ."${dir#/}" != ."$dir" -a -d $dir ]; then 
-		find $dir $FILE_ARGS $IGNORE_FILE_ARGS $REMOVE $VERBOSE
+		echo " * Searching for stale files in: $dir" 
+		find $dir $FILE_ARGS $IGNORE_FILE_ARGS $REMOVE $VERBOSE 
 		find $dir $DIR_ARGS $IGNORE_DIR_ARGS $REMOVE $VERBOSE
+		echo ""
 	fi
-done			 
+done			
+
