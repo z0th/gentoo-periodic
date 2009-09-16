f7be347e129a6b7f6b8d47188fcd0293e0f8c704
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2009-09-16 16:39:06 +0000

    tmp_clean.sh: fixing logic. total rewrite of removal/verbosity logic.

--- trunk/modules/tmp_clean.sh
+++ trunk/modules/tmp_clean.sh
@@ -28,10 +28,12 @@ fi
 # determine if we are removing files
 case "$RM_FILES" in 
 	[yY][eE][sS])	
-		REMOVE="-exec rm {} \;"
+		REMOVE_ARGS="-exec rm {} \;"
+		TF_RM="true"
 	;;
 	[nN][oO])
-		REMOVE=""
+		REMOVE_ARGS=""
+		TF_RM="false"
 	;;
 	*)
 		echo " ERROR! Are we removing files?"
@@ -43,10 +45,12 @@ esac
 # determine verbosicty
 case "$CHATTY" in 
 	[yY][eE][sS])
-		VERBOSE="-print"   
+		VERBOSE_ARGS="-print"  
+		TF_INFO="true"
 	;;
 	[nN][oO])
-		VERBOSE=""
+		VERBOSE_ARGS=""
+		TF_INFO="false"
 	;;
 	*)
 		echo " ERROR! Do you want verbose output?"
@@ -55,23 +59,50 @@ case "$CHATTY" in
 	;; 	
 esac
 
+# so we have four conditionss, one function for each.
+# the noremove_quiet is completely redundant, but included
+# for sanity purposes.
+remove_verbose() {
+	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE
+	find $dir $DIR_ARGS $IGNORE_DIR_ARGS $VERBOSE
+} 
+
+remove_quiet() {
+	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $REMOVE_ARGS 2>&1 >/dev/null
+	find $dir $DIR_ARGS $IGNORE_DIR_ARGS $REMOVE_ARGS 2>&1 >/dev/null 
+}
+
+noremove_verbose() {
+	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE_ARGS	
+	find $dir $DIR_ARGS $IGNORE_DIR_ARGS $VERBOSE_ARGS
+}
+
+noremove_quiet() {
+	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE_ARGS 2>&1 >/dev/null 
+	find $dir $DIR_ARGS $IGNORE_DIR_ARGS 2>&1 >/dev/null
+} 
+
 # file searching/removal done here
+
 for dir in $TMP_ROOT; do 
 	if [ ."${dir#/}" != ."$dir" -a -d $dir ]; then
-		if [ $RM_FILES = "yes" ]; then 	
-			echo " * Searching for stale files in: $dir" 
-			find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE 
-			find $dir $DIR_ARGS $IGNORE_DIR_ARGS $VERBOSE
-			echo ""
-		elif [ $RM_FILES = "no" ]; then 
-			echo " * Removing stale files in: $dir" 
-			find $dir $FILE_ARGS $IGNORE_FILE_ARGS $REMOVE $VERBOSE 
-			find $dir $DIR_ARGS $IGNORE_DIR_ARGS $REMOVE $VERBOSE
-			echo ""
+		
+		if [ $TF_RM = "true" ] && [ $TF_INFO = "true" ]; then
+			echo " * Removing files in: $dir (verbose:ON)"
+			remove_verbose
+		elif [ $TF_RM = "true" ] && [ $TF_INFO = "false" ]; then 
+			echo " * Removing files in: $dir (verbose:OFF)"
+			remove_quiet
+		elif [ $TF_RM = "false" ] && [ $TF_INFO = "true" ]; then
+			echo " * Searching for stale files in: $dir (verbose:ON)"
+			noremove_verbose
+		elif [ $TF_RM = "false" ] && [ $TF_INFO = "false" ]; then
+			echo " * No file removal with no verbosity is redundant!"
 		else
-			echo " * Invalid Option: RM_FILES accepts yes/no option only!"
-			echo "" 
-		fi
+			" * Error! Unanticipated options chosen!"
+			exit 1
+		fi 
+	
+		echo "" 
 	fi
-done			
-
+done
