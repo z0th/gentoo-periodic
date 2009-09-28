a6c1eaec387878b09886bc1a626bc05d70cd8d44
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2009-09-28 20:06:13 +0000

    tmp_clean: full rewrite of tmp_clean.sh module. this version sticks much more closely to the bsd 'clean-tmps' script as far as the logic goes.
    * verbosity configurable (y/n)
    *  file removal configurable (y/n)
    * directory pruning configurable (y/n)
    * output will state if file removal is on/off
    * directory pruning is handled using regex (for find)

--- modules/tmp_clean.sh
+++ modules/tmp_clean.sh
@@ -1,108 +1,92 @@
-#!/bin/bash
-#
-# $Header: /home/drew/bin/projects/gentoo-periodic/modules/RCS/tmp_clean.sh,v 1.5 2009/09/09 18:44:17 drew Exp $
-#
-# tmp_clean.sh - clean out /tmp
-#
+#! /bin/bash
+# remove files? be verbose? 
+REMOVE=no
+VERBOSE=yes
+PRUNED=yes
+# look at dirs? how many days old? 
+SEARCH_DIRS="/var/tmp"
+DAYS=400
+# ignore files and dirs named... 
+IGNORE_NAMES=".X*-lock quota.user quota.group"
+# dont dip down into dirs named...
+# NOTE: this is a find regex and must match the
+# entire path!
+PRUNE_REGEX="/lock\|/etc-update-.*"
+# if enabled, then do stuff, otherwise do nothing.
+ENABLE=yes
 
-# verbosity and file removal on/off
-RM_FILES="no"
-CHATTY="yes"
-# dirs to look at 
-TMP_ROOT="/tmp /var/tmp"
-# number days to ignore, files to ignore
-DAYS=3
-IGNORE_NAMES="( -not -iname "X.*-lock" -o -not -iname "etc-update*" )"
-# args for files
-FILE_ARGS="-depth -type f"
-FILE_ARGS="${FILE_ARGS} -atime +$DAYS -ctime +$DAYS -mtime +$DAYS"
-# args for dirs
-DIR_ARGS="-depth -empty -type d"
-DIR_ARGS="${DIR_ARGS} -atime +$DAYS -ctime +$DAYS -mtime +$DAYS"
-
-if [ -n "${IGNORE_NAMES}" ]; then 
-	IGNORE_FILE_ARGS="${IGNORE_NAMES}"
-	IGNORE_DIR_ARGS="${IGNORE_NAMES}"
-fi
-
-# determine if we are removing files
-case "$RM_FILES" in 
-	[yY][eE][sS])	
-		REMOVE_ARGS="-exec rm {} \;"
-		TF_RM="true"
-	;;
-	[nN][oO])
-		REMOVE_ARGS=""
-		TF_RM="false"
-	;;
-	*)
-		echo " ERROR! Are we removing files?"
-		echo ""
-		exit 1
-	;;
-esac
-
-# determine verbosicty
-case "$CHATTY" in 
-	[yY][eE][sS])
-		VERBOSE_ARGS="-print"  
-		TF_INFO="true"
-	;;
-	[nN][oO])
-		VERBOSE_ARGS=""
-		TF_INFO="false"
-	;;
-	*)
-		echo " ERROR! Do you want verbose output?"
-		echo ""
-		exit 1
-	;; 	
-esac
-
-# so we have four conditionss, one function for each.
-# the noremove_quiet is completely redundant, but included
-# for sanity purposes.
-remove_verbose() {
-	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE
-	find $dir $DIR_ARGS $IGNORE_DIR_ARGS $VERBOSE
-} 
-
-remove_quiet() {
-	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $REMOVE_ARGS 2>&1 >/dev/null
-	find $dir $DIR_ARGS $IGNORE_DIR_ARGS $REMOVE_ARGS 2>&1 >/dev/null 
-}
-
-noremove_verbose() {
-	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE_ARGS	
-	find $dir $DIR_ARGS $IGNORE_DIR_ARGS $VERBOSE_ARGS
-}
-
-noremove_quiet() {
-	find $dir $FILE_ARGS $IGNORE_FILE_ARGS $VERBOSE_ARGS 2>&1 >/dev/null 
-	find $dir $DIR_ARGS $IGNORE_DIR_ARGS 2>&1 >/dev/null
-} 
-
-# file searching/removal done here
-
-for dir in $TMP_ROOT; do 
-	if [ ."${dir#/}" != ."$dir" -a -d $dir ]; then
-		
-		if [ $TF_RM = "true" ] && [ $TF_INFO = "true" ]; then
-			echo " * Removing files in: $dir (verbose:ON)"
-			remove_verbose
-		elif [ $TF_RM = "true" ] && [ $TF_INFO = "false" ]; then 
-			echo " * Removing files in: $dir (verbose:OFF)"
-			remove_quiet
-		elif [ $TF_RM = "false" ] && [ $TF_INFO = "true" ]; then
-			echo " * Searching for stale files in: $dir (verbose:ON)"
-			noremove_verbose
-		elif [ $TF_RM = "false" ] && [ $TF_INFO = "false" ]; then
-			echo " * No file removal with no verbosity is redundant!"
-		else
-			" * Error! Unanticipated options chosen!"
+case "$ENABLE" in
+	[Yy][Ee][Ss])
+		if [ -z $SEARCH_DIRS ]; then
+			echo " * You must set the "SEARCH_DIRS" variable!"
+			if [ -z $DAYS ]; then 
+				echo " * You must set the "DAYS" variable!"
+			fi
 			exit 1
-		fi 
+
+		else 
 	
-		echo "" 
-	fi
-done
+			FIND_ARGS="-atime +$DAYS -mtime +$DAYS -ctime +$DAYS"
+			DIR_ARGS="-empty -mtime +$DAYS"
+
+			if [ -n "$IGNORE_NAMES" ]; then {
+				FIND_ARGS="${FIND_ARGS} "`echo " ${IGNORE_NAMES% }" | \
+				 sed 's/[    ][      ]*/ -not -name /g'`
+				DIR_ARGS="${DIR_ARGS} "`echo " ${IGNORE_NAMES% }" | \
+				 sed 's/[    ][      ]*/ -not -name /g'`
+				}
+			fi
+
+
+			case "$PRUNED" in 
+				[Yy][Ee][Ss])
+					PRUNE="( ! -regex '${PRUNE_REGEX} ) -prune"
+				;;
+				*)
+					PRUNE=
+				;;
+			esac
+
+			# are we removing files? 	
+			case "$REMOVE" in 
+				[Yy][Ee][Ss])
+					RM="-delete"
+				;;
+				*)
+					RM=
+				;;
+			esac
+		
+			# run in verbose mode? 	
+			case "$VERBOSE" in 
+				[Yy][Ee][Ss])
+					PRINT="-print" 
+				;;
+				*)
+					PRINT=
+				;;
+			esac
+			
+			for dir in $SEARCH_DIRS; do 
+				if [ ."${dir#/}" != ."$dir" -a -d $dir ]; then 
+					cd $dir
+					echo " * File removal is set to: $(echo $REMOVE|tr [:lower:] [:upper:])"
+					echo " * Searching $dir for stale files more than $DAYS days old..." 
+					find . -depth -type f $FIND_ARGS $RM $PRINT
+					echo "" 
+					echo " * Searching $dir for stale directories more than $DAYS days old..." 
+					find . -not -name . -type d $PRUNE $DIR_ARGS $RM $PRINT
+					echo "" 
+				fi 
+			done
+	
+		fi 
+	;;
+	*)
+		echo " * ENABLED is set to NO."
+		echo " * No cleaning of temporary directories preformed!"
+		echo ""
+		exit 1
+	;;
+esac
+
