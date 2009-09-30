f8eb57ce66cfac839483c7f45de59d1cbd3cb888
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2009-09-30 15:40:03 +0000

    file_bkup: * fixed some spelling. * a few minor touch-ups

--- modules/file_bkup.sh
+++ modules/file_bkup.sh
@@ -2,17 +2,17 @@
 
 # $Header: /home/drew/bin/projects/modules/RCS/file_bkup.sh,v 1.1 2009/09/09 20:02:01 drew Exp $
 
-# file_bkup.sh - back up critical system files.
+# file_bkup.sh - back up critical system files and directories.
 
-# default files to be backed up
+# default files/dirs to be backed up
 def_bkup="/etc/passwd /etc/group /etc/shadow /etc/gshadow"
-# optional files to be backed up
+# optional files/dirs to be backed up
 opt_bkup="/etc/hosts /etc/make.conf /etc/mdadm.conf"
 # backup destination
 BKUP_DEST="/data/backup/daily-backup"
 # be verbose? 
 VERBOSE=no
-#
+# cating vars together 
 BKUP_FILES="${def_bkup} ${opt_bkup}"
 # flags to hand to cp 
 CP="cp --preserve=all --parents --no-dereference --update"
@@ -26,8 +26,8 @@ case ${VERBOSE} in
 	;;
 esac 
 
-# copy files f'n, if a dir then copy recursive
-# if a file then just copy, otherwise exit.
+# copy files f'n, if a dir then copy recursive;
+# if a file then just copy; otherwise exit.
 copy_files() {
 	for file in $BKUP_FILES; do
 		if [ -d $file ]; then 
@@ -42,16 +42,16 @@ copy_files() {
 	done
 }
 
-# make sure dest dir exists then copy. 
+# make sure dest dir exists, if not then create and/or copy. 
 if [ -d $BKUP_DEST ]; then
-	echo " * Copying system files to: $BKUP_DEST"
+	echo " * Copying files to: $BKUP_DEST"
 	copy_files
 	echo ""
 else
-	echo " * Destionation does not exist! Creating..."
+	echo " * Destination $BKUP_DEST does not exist! Creating..."
 	mkdir -p $BKUP_DEST
-	echo " * Copying system files to: $BKUP_DEST"
+	echo " * Copying files to: $BKUP_DEST"
 	copy_files
 	echo ""
 fi
-	   
+   
