016ba7ddfb454727a5ec0e064d768bc8027ec2bd
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2009-10-14 19:24:01 +0000

    tmp_clean: * fixed missing quote in PRUNED case. * added header to file removal section.
    file_bkup: 
    * removed some redundant informationals.
    
    tmpreaper: 
    * fixed a varabile spelling error. 
    * got execution vars into larrger case.
    * added ENABLE=yes/no var.
    * working on getting --protect 'expr' working properly.

--- modules/tmpreaper.sh
+++ modules/tmpreaper.sh
@@ -2,10 +2,13 @@
 
 # TMPEAPER.SH - alternate method of removing files from temporary directories.
 
+# am i enabled? 
+ENABLE="YES"
+
 # target directories 
-tmp_dirs="/tmp /test" 
+target="/tmp" 
 # days old
-time_spec=375d
+grace_period=375d
 # verbosity
 verbosity=NO
 # test only, do not remove files
@@ -19,51 +22,77 @@ protect_files=".X*-lock quota.user quota.group *.pid etc-update-*"
 
 # locate tmpreaper on the system.
 tmpreap=$(which tmpreaper)
-
-# before doing anything, make sure its installed.
 if [ -z $tmpreap ]; then 
 	echo " * tmpreaper is not installed!"
-	echo " Please install app-admin/tmpreaper!"
+	echo " * Please install app-admin/tmpreaper!"
 	echo "" 
 	exit 1 
 fi 
+# make sure something dumb isnt happening.
+if [ $target = / ]; then 
+	echo " * You should not run this on the root directory!"
+	exit 1
+fi 
+ 
+case "$ENABLE" in
+	[Yy][Ee][Ss])
+		# verbosity 
+		case "${verbosity}" in
+			[yY][eE][Ss])
+				verbose="--verbose"
+			;;
+			*)
+				verbose=
+			;;
+		esac
+		# test only
+		case "${test_only}" in 
+			[yY][eE][Ss])
+				test="--test"
+			;; 
+			*)
+				test=
+			;; 
+		esac
+		# show removed? 
+		case "${show_rm}" in 
+			[yY][eE][Ss])
+				showrm="--showdeleted"
+			;; 
+			*)
+				showrm= 
+			;; 
+		esac 
+		# grace period
+		if [ -n ${grace_period} ]; then 
+			grace=${grace_period}
+		else
+			echo " * Setting of grace_period mandatory! Exiting!"
+			exit 1 
+		fi
+		# max run time	
+		if [ -n ${max_run} ]; then 
+			runtime="--runtime=$max_run"
+		else 
+			runtime=
+		fi
+		# file type protection	
+		if [ -n "${protect_files}" ]; then 
+			for expr in $protect_files; do 
+				protect="${protect} --protect $expr"
+			done
+		else 
+			protect=
+		fi
 
-# verbosity 
-case "${verboxity}" in
-	[yY][eE][Ss])
-		verbose="--verbose"
+		# executing tmpreaper command, with flags 
+		$tmpreap $verbose $test $showrm $runtime $protect $grace $target 	
 	;;
+
 	*)
-		verbose=
+		# disabled, or broken
+		echo " * ENABLES is set to NO or invalid option."
+		echo " * tmpreaper module did not execute."
 	;;
 esac
-
-# test only
-case "${test_only}" in 
-	[yY][eE][Ss])
-		test="--test"
-	;; 
-	*)
-		test=
-	;; 
-esac
-
-case "${show_rm}" in 
-	[yY][eE][Ss])
-		showrm="--showdeleted"
-	;; 
-	*)
-		showrm= 
-	;; 
-esac 
-
-if [ -n ${max_run} ]; then 
-	runtime="--runtime=$max_run"
-else 
-	runtime=
-fi
-
-if [ -n $(time_spec)]; then 
-	time=$time_spec
-fi
 
