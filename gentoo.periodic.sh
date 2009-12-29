4a882c9be965dc71e4b353c7e17f182ba79165ac
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2009-12-29 21:09:22 +0000

    ** removed modules/disk_usage.sh. function is handled by basic_sysinfo.sh. redundant. ** updates to gentoo-periodic.sh. comments. added self_ref f'n to extract path of install and export to subshells. in preparation for moving various variable settings into a singular .conf file.

--- gentoo.periodic.sh
+++ gentoo.periodic.sh
@@ -1,77 +1,97 @@
 #!/bin/bash
 #
-# $Header: /usr/local/sbin/gentoo.periodic/RCS/gentoo.periodic.sh,v 1.7 2009/02/19 20:18:53 root Exp $
-#
-# gentoo.peroidic - run scripts in given dirs. this is 
+# gentoo.peroidic.sh - run scripts in given dirs. this is 
 # designed to run out of crontab. 
 
-# base directory where gentoo.periodic is installed
-BASE_DIR="/usr/local/sbin/gentoo.periodic"
+# simple usage statement
+output_usage() {
+        echo " Usage: generate-reports [global-option]"
+        echo " Global Options:"
+        echo "   -c: run script"
+        echo "   -d: daily"
+        echo "   -w: weekly"
+        echo "   -m: monthly"
+        echo "   -y: yearly"
+        echo "   -s: security"
+        echo "   -h: this help message"
+}
+
+# this actually executes the script modules
+run_script() {
+        for script in `ls -1 $BASE_DIR/$PERIOD | sort -n`; do
+                sh $SCRIPTPATH/$PERIOD/$script >> $TMP_FILE
+        done
+}
+
+# base directory where gentoo.periodic is installed, EXPORT.
+self_ref() {
+        # full path to this script.
+        SCRIPT=$(readlink -f $0)
+        # path to install dir.
+        SCRIPTPATH=`dirname $SCRIPT`
+        export SCRIPT 
+        export SCRIPTPATH
+}
+
+# LOCAL VARIABLES! 
+# NOTE: some of these will need to be pulled out and sourced
+# from a .conf file [$BASE_DIR/gentoo-periodic.conf]. 
+#
+# base directory where gentoo.periodic is installed, LOCAL.
+BASE_DIR=$(readlink -f $0|xargs dirname)
+#BASE_DIR="/usr/local/sbin/gentoo.periodic"
+# destination email address
 DST_EML=""
+# todays date
 NOW=$(date +%F)
 # temp file to output to, exported to the shell
 TMP_FILE="/tmp/$$.perioidic.tmp"
 export TMP_FILE
 
-# simple usage statement
-output_usage() {
-	echo " Usage: generate-reports [global-option]"
-	echo " Global Options:"
-	echo "   -c: run script"
-	echo "   -d: daily"
-	echo "   -w: weekly"
-	echo "   -m: monthly"
-	echo "   -y: yearly"
-	echo "   -s: security"
-	echo "   -h: this help message"
-}
-
-# this actually executes the script module 
-run_script() {
-	for script in `ls -1 $BASE_DIR/$PERIOD | sort -n`; do
-		sh $BASE_DIR/$PERIOD/$script >> $TMP_FILE
-	done
-}
-
 # option list and script runner handling. 
 while getopts c:dmwysh OPT; do
-	case "$OPT" in 
-		c)	# run script manually 	
-			SCRIPT="$OPTARG"
-			sh -x $SCRIPT 
-			;;
-		d)	# daily
-			PERIOD="daily"
-			run_script
-			;;
-		w)	# weekly
-			PERIOD="weekly"
-			run_script
-			;;
-		m)	# monthly
-			PERIOD="monthly"
-			run_script
-			;;
-		y)	# yearly
-			PERIOD="yearly"
-			run_script
-			;;
-		s) 	# security
-			PERIOD="security"
-			run_script
-			;;
-		h)	# help
-			output_usage
-			;;
-		*)	# everything else
-			output_usage
-			;;
-	esac
+        case "$OPT" in 
+                c)      # run script manually 
+                        SCRIPT="$OPTARG"
+                        sh -x $SCRIPT 
+                        ;;
+                d)      # daily
+                        PERIOD="daily"
+                        self_ref
+                        run_script
+                        ;;
+                w)      # weekly
+                        PERIOD="weekly"
+                        self_ref
+                        run_script
+                        ;;
+                m)      # monthly
+                        PERIOD="monthly"
+                        self_ref
+                        run_script
+                        ;;
+                y)      # yearly
+                        PERIOD="yearly"
+                        self_ref
+                        run_script
+                        ;;
+                s)      # security
+                        PERIOD="security"
+                        self_ref
+                        run_script
+                        ;;
+                h)      # help
+                        output_usage
+                        ;;
+                *)      # everything else
+                        output_usage
+                        ;;
+        esac
 done
 
 # mailout and housekeeping.
 HOST=`hostname | tr [:lower:] [:upper:]`
 if [[ -e $TMP_FILE ]]; then 
-	mail -s "$HOST: $PERIOD report for $NOW" $DST_EML < $TMP_FILE
-	rm -f $TMP_FILE
+        mail -s "$HOST: $PERIOD report for $NOW" $DST_EML < $TMP_FILE
+        rm -f $TMP_FILE
 fi 
