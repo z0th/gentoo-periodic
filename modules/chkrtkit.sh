0bba5c9f6432e03a2ea5d1f111ab8ae039fb90a1
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2010-04-19 22:13:54 +0000

    chkrootkit.sh: corrected some code. added "you should be using rkhunter" message.

--- modules/chkrtkit.sh
+++ modules/chkrtkit.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #
-# $Header: /usr/local/sbin/gentoo.periodic/modules/RCS/chkrtkit.sh,v 1.2 2008/04/04 18:59:46 root Exp $
-#
 # chkrootkit.sh - check for presence of rootkits on the system.
 #
-CHKRK=`whereis -b -B /usr/sbin/ -f chkrootkit|awk -F\: '{print $2}'|sed 's/^[ ]*//'`
+# where is it installed...
+CHKRK=$( whereis -b -B /usr/sbin/ -f chkrootkit|awk -F\: '{print $2}'|sed 's/^[ ]*//' )
+#
+# dir structure to start check at.
+chkrtkit_dir_start="/"
 
+# where the real work happens
 if [[ -z $CHKRK ]]; then 
 	echo " * chkrootkit is not installed!"
 	echo " * Please install app-forensics/chkrootkit!"
 	echo ""
 else
-	$CHKRK -n -r /
+	echo " * You should probably be using rkhunter instead!" 
+	$CHKRK -n -r $chkrtkit_dir_start 
 	echo ""
 fi
 
