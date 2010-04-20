1f1e31e539f36d42d99c29e68229aab716791f0a
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2010-04-20 13:06:18 +0000

    ssh_login_v2.sh: awking of wrong field fixed.

--- modules/ssh_login_v2.sh
+++ modules/ssh_login_v2.sh
@@ -27,7 +27,7 @@ fgrep "POSSIBLE BREAK-IN ATTEMPT" $SSH_TMP_FILE | grep -v "but this does not map
 | awk '{print $12}' | sed 's/\[\|\]//g' >> $SSH_CNT_FILE
 # there are two types of these BREAK IN lines
 fgrep "POSSIBLE BREAK-IN ATTEMPT" $SSH_TMP_FILE | grep -v "reverse mapping checking getaddrinfo for" \
-| awk '{print $12}' | sed 's/\[\|\]//g' >> $SSH_CNT_FILE
+| awk '{print $7}' | sed 's/\[\|\]//g' >> $SSH_CNT_FILE
 # count them up
 cat $SSH_CNT_FILE | sortip | uniq -c 
 echo ""
