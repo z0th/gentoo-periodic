a8b7d17b1e592b618078ebc1864173bde35ba327
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2010-04-21 17:12:35 +0000

    eclean.sh: added config sourcing. moved vars off to config.

--- trunk/modules/eclean.sh
+++ trunk/modules/eclean.sh
@@ -3,7 +3,20 @@
 # $Header: /usr/local/sbin/gentoo.periodic/modules/RCS/eclean.sh,v 1.2 2008/06/07 17:38:39 root Exp $
 #
 # eclean.sh - check to see if the portage tree needs cleaning
-#
+
+# ----------------
+# this code sources the config file! must be at the top of every module! 
+my_name=$(basename $0)
+# source config
+if [ -e $INSTALL_DIR/gentoo.periodic.conf ]; then
+	. $INSTALL_DIR/gentoo.periodic.conf 
+else 
+	echo " * ERROR:" $my_name ": cannot source config!"
+	exit 0
+fi
+# ----------------
+
+# check cleanlieness of portage tree
 echo " * Checking the cleanlieness of the Portage Tree..."
 echo " * running eclean-dist..." 
 /usr/bin/eclean-dist --nocolor --pretend 2>&1 
