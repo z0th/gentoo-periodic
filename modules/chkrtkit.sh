df49551536e7bf251dbe4d3429f4829d35f9adee
dead.node <dead.node@5f08c0da-9b31-11de-bf47-b33d4a03a5a8>
2010-04-21 17:07:58 +0000

    chkrtkit.sh: added config sourcing. moved vars off to config.

--- modules/chkrtkit.sh
+++ modules/chkrtkit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #
-# chkrootkit.sh - check for presence of rootkits on the system.
-#
-# where is it installed...
+# chkrtkit.sh - check for presence of rootkits on the system.
+
+# ----------------
+# this code sources the config file! must be at the top of every module! 
+my_name=$(basename $0)
+# source config
+if [ -e $INSTALL_DIR/gentoo.periodic.conf ]; then
+   . $INSTALL_DIR/gentoo.periodic.conf 
+else 
+   echo " * ERROR:" $my_name ": cannot source config!"
+   exit 0
+fi
+# ----------------
+
+# where is chkrootkit installed...
 CHKRK=$( whereis -b -B /usr/sbin/ -f chkrootkit|awk -F\: '{print $2}'|sed 's/^[ ]*//' )
 #
 # dir structure to start check at.
